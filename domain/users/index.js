4e8e2b9ec09e030d86b40b32aa32d68aac8b4cc5
Fedor Endovitskiy <rusfjord@ulteam8.ru>
2019-08-03 19:15:38 +0300

    Added domain layer and user repository

---
+++ domain/users/index.js
@@ -0,0 +1,21 @@
+module.exports = {
+	findAllUsers: () => {
+		return [
+			{
+				"id": "1",
+				"login": "admin@site.com",
+				"name": "Администратор"
+			},
+			{
+				"id": "2",
+				"login": "user1@site.com",
+				"name": "Пользователь 1"
+			},
+			{
+				"id": "3",
+				"login": "user2@site.com",
+				"name": "Пользователь 2"
+			}
+		];
+	}
+};
