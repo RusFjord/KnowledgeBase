ba0c0fb4698e7ed98f07a23ee31af6476f23b0c9
Fedor Endovitskiy <f.endovitskiy@ulteam8.ru>
2019-08-05 16:37:24 +0300

    Included mongoose and body-parser. Added path to db in config object

--- app.js
+++ app.js
@@ -1,5 +1,11 @@
 const express = require('express');
+const mongoose = require('mongoose');
+const bodyParser = require("body-parser");
+
 const config = require('./config');
+
+mongoose.connect(config.db.path, {useNewUrlParser: true });
+
 
 //routes
 const index = require('./routes/index');
@@ -7,6 +13,8 @@ const users = require('./routes/users');
 const err404 = require('./routes/err404');
 
 const app = express();
+
+app.use(bodyParser.urlencoded({extended: false}));
 
 app.use('/api_v_1_0', index);
 app.use('/api_v_1_0/users', users);
--- config.js
+++ config.js
@@ -1,5 +1,8 @@
 module.exports = {
 	port: 3000,
+	db: {
+		path: 'mongodb://know:far37Nit@ds259367.mlab.com:59367/knowbase'
+	},
 	info: {
 		SERVER_STARTED: 'API server started'
 	}
