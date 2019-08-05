e773ae2f72473c7aa850b9ea2e15987e9d9a8d67
Fedor Endovitskiy <f.endovitskiy@ulteam8.ru>
2019-08-05 16:40:05 +0300

    Added routes post and changed route get.

--- routes/users.js
+++ routes/users.js
@@ -3,7 +3,39 @@ const express = require('express');
 const router = express.Router();
 
 router.get('/', (req, res) => {
-	res.send(users.findAllUsers());
+	const answer = users.findAllUsers();
+	answer.exec((err, users) => {
+		 	if (err) {
+		 		console.log(err);
+		 		const error = {
+		 			name: 'Ошибка запроса к базе данных',
+					description: err
+				};
+		 		res.send(error);
+		 	}
+
+		 	res.send(users);
+	});
+});
+
+router.post('/', (req, res) => {
+	const user = {
+		login: 'admin@site.com',
+		pass: '',
+		name: 'Администратор'
+	};
+	const userQuery = users.create(user);
+	userQuery.then((err) => {
+		if (err) {
+			console.log(err);
+			const error = {
+				name: 'Ошибка запроса к базе данных',
+				description: err
+			};
+			res.send(error);
+		}
+		res.send({result: 'success'});
+	});
 });
 
 module.exports = router;
