dc0ae59792ea560608b4b7c74ea454ae57cf9514
Fedor Endovitskiy <rusfjord@ulteam8.ru>
2019-08-07 06:18:04 +0300

    Added CRUD-routes for users

--- routes/users.js
+++ routes/users.js
@@ -7,8 +7,23 @@ router.get('/', async (req, res) => {
 	res.send(answer)
 });
 
+router.get('/:id', async (req, res) => {
+	const answer = await users.read({_id: req.params.id});
+	res.send(answer)
+});
+
 router.post('/', async (req, res) => {
 	const answer = await users.create(req.body);
+	res.send(answer);
+});
+
+router.put('/:id', async (req, res) => {
+	const answer = await users.update(req.params.id, req.body);
+	res.send(answer);
+});
+
+router.delete('/:id', async (req, res) => {
+	const answer = await users.delete(req.params.id);
 	res.send(answer);
 });
 
