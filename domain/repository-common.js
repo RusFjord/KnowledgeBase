450679d51097cad18a57c86136be102c25e1a095
Fedor Endovitskiy <f.endovitskiy@ulteam8.ru>
2019-08-09 09:23:03 +0300

    Fixed repository function

--- domain/repository-common.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-    error: {
-        description: "Database error",
-        data: null
-    },
-    success: {
-        result: 'success'
-    }
-};
--- domain/repository.js
+++ domain/repository.js
@@ -1,21 +1,23 @@
-const common = require('./repository-common');
+const error = {
+    description: "Database error",
+    data: null
+};
 
 module.exports = {
     create: async (Model, data) => {
         const current = new Model(data);
-        return current.save().then((err) => {
+        return current.save((err) => {
             if (err) {
-                common.error.data = err;
-                return common.error;
+                error.data = err;
+                return error;
             }
-            return common.success;
         });
     },
     read: async (Model, query) => {
         return Model.find(query, (err, result) => {
             if (err) {
-                common.error.data = err;
-                return common.error;
+                error.data = err;
+                return error;
             }
             return result;
         });
@@ -23,19 +25,17 @@ module.exports = {
     update: async (Model, id, data) => {
         return Model.findByIdAndUpdate( id, data,(err) => {
             if (err) {
-                common.error.data = err;
-                return common.error;
+                error.data = err;
+                return error;
             }
-            return common.success;
         });
     },
     delete: async (Model, id) => {
         return Model.findByIdAndDelete( id, (err) => {
             if (err) {
-                common.error.data = err;
-                return common.error;
+                error.data = err;
+                return error;
             }
-            return common.success;
         });
     }
 };
