f77f55e883548983cbd0d5c3339f3a74433fc5de
Fedor Endovitskiy <f.endovitskiy@ulteam8.ru>
2019-08-09 09:44:10 +0300

    Changed users API

--- domain/model-factory.js
+++ domain/model-factory.js
@@ -2,7 +2,8 @@ const models = new Map([
 	['user_roles', require('./user_roles/model')],
 	['tags', require('./tags/model')],
 	['roles', require('./roles/model')],
-	['categories', require('./categories/model')]
+	['categories', require('./categories/model')],
+	['users', require('./users/model')]
 	]
 );
 
